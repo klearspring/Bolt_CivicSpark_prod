6cf164461219a4cc987fce660581dec11b93cca4
klearspring <klearspring@gmail.com>
2025-06-27 18:04:30 -0600

    Update BoltHackathonBadge to use SVG file directly

--- components/BoltHackathonBadge.tsx
+++ components/BoltHackathonBadge.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import { View, StyleSheet, TouchableOpacity, Linking, Platform } from 'react-native';
-import { SvgXml } from 'react-native-svg';
+import { SvgUri } from 'react-native-svg';
 
 interface BoltHackathonBadgeProps {
   width?: number;
   height?: number;
   style?: any;
 }
-
-// Import the SVG as a string
-const boltBadgeSvg = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0.00 0.00 360.00 360.00">
-<circle cx="180" cy="180" r="180" fill="#000000"/>
-</svg>`;
 
 export default function BoltHackathonBadge({ 
   width = 120, 
@@ -49,10 +42,10 @@ export default function BoltHackathonBadge({
       onPress={handlePress}
       activeOpacity={0.8}
     >
-      <SvgXml 
-        xml={boltBadgeSvg} 
-        width={badgeSize} 
-        height={badgeSize} 
+      <SvgUri
+        width={badgeSize}
+        height={badgeSize}
+        uri="/assets/images/bolt-badge.svg"
       />
     </TouchableOpacity>
   );
